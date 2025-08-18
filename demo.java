e2366f0259c91f6993c1bece2b76b6cfb73a1c18
Shriwayanta <shri.rivu@gmail.com>
2025-08-19 00:35:24 +0530

    .

--- demo.java
+++ demo.java
@@ -1,5 +1,7 @@
 public class demo{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Java");
+        // sout(tab) = System.out.println
+        // difference between print and println is that println leaves a line whereas print don't
     }
 }
