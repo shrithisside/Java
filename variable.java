9730990404478d6976422abbd751113291b11173
Shriwayanta <shri.rivu@gmail.com>
2025-08-19 01:01:22 +0530

    .

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        // Variable
+        String name = "Aman";
+        String neighbour = "Akku";
+        int age = 30;
+        String friend  = neighbour;
+        System.out.println("Friend: " + friend);
+        System.out.println("Age: " + age);
+    }
+}
