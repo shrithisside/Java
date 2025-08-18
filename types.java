a849784d94c86e3b128b9d4db488852e7a9be39f
Shriwayanta <shri.rivu@gmail.com>
2025-08-19 01:14:13 +0530

    .

---
+++ types.java
@@ -0,0 +1,23 @@
+public class types {
+    public static void main(String[] args) {
+        // PRIMITIVE TYPES:
+        // byte - 1 [ -128 to 127]
+        // short - 2
+        // int - 4 [1,2,3 ....]
+        // long - 8
+        // float - 4
+        // double - 8
+        // char - 2 [a,b,c,d.....]
+        // boolean - 1 [true or false]
+        byte age = 30;
+        int phone = 8910;
+        long phone2 = 8910944852L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean isAdult = true;
+
+        //NON PRIMITIVE TYPES:
+        String name = "Shriwayanta Maiti";
+        System.out.println(name.length());
+    }
+}
