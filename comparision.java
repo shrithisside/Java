0822389c46de8dd64099bd99ca51e06826ec39f3
Shriwayanta <shrithissidee@gmail.com>
2025-08-20 02:17:00 +0530

    .

---
+++ comparision.java
@@ -0,0 +1,5 @@
+public class comparision {
+    public static void main(String[] args) {
+        //comparision operators
+    }
+}
--- input.java
+++ input.java
@@ -6,5 +6,18 @@ public class input {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int num = sc.nextInt();
+        System.out.println("your no. is " + num);
+        System.out.println("Enter your age");
+        float age = sc.nextFloat();
+        System.out.println("your age is " + age);
+
+
+        System.out.println("Enter your name");
+        String name = sc.next();// here next only takes a single word even if we input a complete line
+        System.out.println("your name is " + name);
+
+        System.out.println("Enter your surname");
+        String surname = sc.nextLine(); // this is used to take input for a line
+        System.out.println("your surname is " + surname);
     }
 }
