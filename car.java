1831563161ca42df092ed45a506cdf5324587146
Shriwayanta <shrithissidee@gmail.com>
2025-08-20 14:41:03 +0530

    .

--- car.java
+++ car.java
@@ -9,13 +9,13 @@ public class car {
 
     //create a speed() method and a parameter
     public void speed(int maxSpeed){
-        System.out.println("Max speed is " + maxSpeed);
+        System.out.print("Max speed is " + maxSpeed);
     }
 
     //Inside main, call the method on my car object
     public static void main(String[] args){
         car myCar = new car();          // create myCar object
         myCar.fullthrottle();           // call the fullthrottle() method
-        myCar.speed(200);       // call the speed method
+        myCar.speed(200);               // call the speed method
     }
 }
