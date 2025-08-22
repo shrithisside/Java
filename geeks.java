941c4e4bc20cec7a7f7554f7f94c4dbccf055366
Shriwayanta <shrithissidee@gmail.com>
2025-08-22 10:58:46 +0530

    .

---
+++ geeks.java
@@ -0,0 +1,12 @@
+public class geeks {
+    int x;
+
+    public geeks() {
+        x=10;
+    }
+
+    public static void main(String[] args) {
+        geeks obj = new geeks();
+        System.out.println(obj.x);
+    }
+}
---
+++ geeks1.java
@@ -0,0 +1,15 @@
+public class geeks1 {
+
+    String name;
+    int id;
+
+    geeks1(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        geeks1 obj = new geeks1("Tambi", 68);
+        System.out.println("Name : " + obj.name + " and ID : " + obj.id);
+    }
+}
