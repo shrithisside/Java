95cdbce289bbb5ebf8e55783fd18970e5d0b20f9
Shriwayanta <95484092+Shriwayanta@users.noreply.github.com>
2025-09-06 21:25:27 +0530

    Update Armstrong.java

--- codes/lecture_codes/Armstrong.java
+++ codes/lecture_codes/Armstrong.java
@@ -5,38 +5,39 @@ package codes.lecture_codes;
 
 import java.util.Scanner;
 
-class ArmstrongDemo {
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
 
-    // Constructor to check Armstrong number
-    ArmstrongDemo(int num) {
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    public static boolean isArmstrong(int num) {
         int originalNum = num;
         int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        System.out.println("Number of digits: " + digits);
+        System.out.println("Calculating sum of each digit raised to the power " + digits + ":");
 
         while (num > 0) {
-            int digit = num % 10;   // extract last digit
-            sum += digit * digit * digit;  // cube and add
-            num = num / 10;        // remove last digit
+            int digit = num % 10;
+            double powered = Math.pow(digit, digits);
+            System.out.println("Digit: " + digit + " -> " + digit + "^" + digits + " = " + (int)powered);
+            sum += powered;
+            num /= 10;
         }
 
-        if (sum == originalNum) {
-            System.out.println(originalNum + " is an Armstrong number.");
-        } else {
-            System.out.println(originalNum + " is NOT an Armstrong number.");
-        }
+        System.out.println("Sum of powered digits: " + sum);
+        System.out.println("Comparing sum with original number: " + sum + " == " + originalNum);
+
+        return sum == originalNum;
     }
 }
-
-public class Armstrong {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        // Calling constructor (object creation triggers Armstrong check)
-        ArmstrongDemo obj = new ArmstrongDemo(n);
-
-        sc.close();
-    }
-}
-
