1c138a1cc5648b1b5a45088a741e5bd741056e81
Shriwayanta <shrithissidee@gmail.com>
2025-09-11 14:39:28 +0530

    .

--- codes/lecture_codes/circle.java
+++ codes/lecture_codes/circle.java
@@ -6,14 +6,11 @@ public class circle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input radius
         System.out.print("Enter the radius of the circle : ");
         double radius = sc.nextDouble();
 
-        // Calculate area
         double area = Math.PI * radius * radius;
 
-        // Output result
         System.out.println("The area of the circle is: " + area);
 
         sc.close();
