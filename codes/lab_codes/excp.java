0a6fa7dc7675c0db1a824924e9c7b33dc0ce7e73
Shriwayanta <shrithissidee@gmail.com>
2025-11-01 11:57:57 +0530

    lab 6

---
+++ codes/lab_codes/excp.java
@@ -0,0 +1,16 @@
+class Excp{
+    public static void main(String args[])
+    {
+        try{
+            int[] array = {2,4,5};
+
+            System.out.println(array[5]);
+        }
+        catch(Exception e){
+            System.out.println("Exception class = "+e.getClass());
+        }
+        finally{
+            System.out.println("This block will always be executed");
+        }
+    }
+}
--- codes/lab_codes/student.java
+++ codes/lab_codes/student.java
@@ -1,18 +1,40 @@
 package mypkg1;
-import java.util.scanner;
-public class student{
+import java.util.Scanner;
+public class student
+{
     int id;
     String name;
-    public student(){
-        name="java"
+    public student() {
+        name="java";
     }
 
-    public student(int i, String nm){
+    public student(int i,String nm)
+    {
         id=i;
         name=nm;
     }
+    public void details()
+    {
+        System.out.println("StudentDetails id="+id+" name="+name);
+    }
+    public static void main(String args[])
+    {
+        int id=0;
+        String name="";
+        Scanner sc=new Scanner(System.in);
 
-    public void details(){
-        
+        System.out.println("Enter id=");
+        id=sc.nextInt();
+
+        System.out.println("Enter Name=");
+        name=sc.next();
+
+        student s=new student(id,name);
+        s.details();
+
+
+
+
+
     }
 }
