8f5faec108fc85e7b4b4bff3e649a337c0aaff86
Shriwayanta <shrithissidee@gmail.com>
2025-11-01 11:43:21 +0530

    updates

---
+++ codes/lab_codes/student.java
@@ -0,0 +1,18 @@
+package mypkg1;
+import java.util.scanner;
+public class student{
+    int id;
+    String name;
+    public student(){
+        name="java"
+    }
+
+    public student(int i, String nm){
+        id=i;
+        name=nm;
+    }
+
+    public void details(){
+        
+    }
+}
